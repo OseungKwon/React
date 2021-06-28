d62f10eb5827118c783d53267cf79676670ed566
OseungKwon <00osos@naver.com>
2021-06-29 06:48:04 +0900

    add

---
+++ practice/src/MyComponent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MyComponent() {
+    return (
+        <div>
+            <ul>
+                <li>a</li>
+                <li>b</li>
+                <li>c</li>
+                <li>d</li>
+                <li>e</li>
+                <li>f</li>
+            </ul>
+        </div>
+    )
+}
+export default MyComponent;
